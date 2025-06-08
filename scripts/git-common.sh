68bc0413564e3edb8d562d170a6411faf7c40467
jdetok <jdeko17@gmail.com>
2025-06-08 15:02:40 -0500

    msg

--- scripts/git-common.sh
+++ scripts/git-common.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-read -p "msg: " msg
+msg=""
+
+while getopts "m:" opt; do
+    case $opt in
+        m) msg="$OPTARG" ;;
+        *) read -p "msg: " msg 
+    esac 
+done
+
+
+if [ -z "$msg" ]; then
+    read -p "msg: " msg
+fi 
+
+
 git add . && \
 git commit -m "$msg" && \
 git push
